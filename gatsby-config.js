455038ceae929fef74b26be1905c5adb6fdea621
Roberth Strand <roberth.strand@amesto.no>
2022-12-05 14:56:27 +0100

    feat: initial blog test
    Signed-off-by: Roberth Strand <roberth.strand@amesto.no>

---
+++ gatsby-config.js
@@ -0,0 +1,148 @@
+const path = require("path")
+
+module.exports = {
+  pathPrefix: `/gatsby-theme-pitayan`,
+  siteMetadata: {
+    title: `Amesto Fortytwo`,
+    name: `Fortytwo blog`,
+    description:"THE ANSWER IS MEANINGLESS - unless you understand the question.",
+    siteUrl: `https://amestofortytwo.github.io/blog`,
+    siteSlogan: `THE ANSWER IS MEANINGLESS - unless you understand the question.`,
+    siteLinks: [
+      {
+        name: "sitemap",
+        url: "/sitemap/sitemap-index.xml",
+        group: "site",
+        internal: true,
+      },
+      {
+        name: "rss",
+        url: "/rss.xml",
+        group: "site",
+        internal: true,
+      },
+      {
+        name: "about",
+        url: "/about",
+        group: "site,home",
+        internal: true,
+      },
+      {
+        name: "email",
+        url: "mailto: hello@amestofortytwo.com",
+        group: "connect",
+        internal: false,
+      },
+    ],
+    siteCookieConsent: {
+      title: '',
+      description: '',
+      readMore: '',
+    },
+    siteSubscription: {
+      title: '',
+      description: '',
+    },
+  },
+  plugins: [
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({
+              query: {
+                site: {
+                  siteMetadata: { siteUrl },
+                },
+                allMdx,
+              },
+            }) => {
+              return allMdx.nodes.map(node => {
+                return Object.assign({}, node.frontmatter, {
+                  url: siteUrl + node.fields.slug,
+                  guid: siteUrl + node.fields.slug,
+                  custom_elements: [
+                    {
+                      "content:encoded": node.html,
+                    },
+                    {
+                      tags: node.frontmatter.categories.join(","),
+                    },
+                    {
+                      featuredImage: siteUrl + node.frontmatter.hero.publicURL,
+                    },
+                  ],
+                })
+              })
+            },
+            query: `
+              {
+                allMdx(
+                  filter: { fileAbsolutePath: { regex: "/content/posts/" } }
+                  sort: { order: DESC, fields: [frontmatter___date] }
+                ) {
+                  nodes {
+                    html
+                    mdxAST
+                    fields {
+                      slug
+                    }
+                    frontmatter {
+                      hero {
+                        publicURL
+                      }
+                      categories
+                      date
+                      title
+                      description
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            match: "^/posts/",
+            title: "Amesto Fortytwo RSS Feed",
+          },
+        ],
+      },
+    },
+    {
+      resolve: `@pitayan/gatsby-theme-pitayan`,
+      options: {
+        siteAssets: "src/assets",
+        postsPerPage: 6,
+        mailChimpEndpoint:
+          "https://pitayanblog.us14.list-manage.com/subscribe/post?u=234bf6777b76872feb7d92a68&amp;id=27fad95f3b",
+        mailChimpTimeout: 3500,
+        applyGatsbyRemarkPlugins: defaultPlugins => [
+          {
+            resolve: `@raae/gatsby-remark-oembed`,
+            options: {
+              usePrefix: ["oembed"],
+              providers: {
+                exclude: ["Redit"],
+              },
+            },
+          },
+          `gatsby-remark-responsive-iframe`,
+          ...defaultPlugins,
+        ],
+      },
+    },
+  ],
+}
---
+++ postcss.config.js
@@ -0,0 +1,1 @@
+module.exports = require("@pitayan/gatsby-theme-pitayan/postcss.config")
