7989d17ea4bc9baa84b55d8d5e6b5c929907abba
remi.vandemir <remi.vandemir@amesto.no>
2023-02-14 10:54:52 +0100

    fixed some typos
    Signed-off-by: remi.vandemir <remi.vandemir@amesto.no>

---
+++ .devcontainer 2/post-create.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Install gatsby en react
+#sudo npm install --save-dev gatsby @pitayan/gatsby-theme-pitayan react@18 react-dom@18 --legacy-peer-deps
+
+#sudo npm install
+#sudo npm update
+
+# create a new Gatsby site using the blog starter
+#gatsby new my-blog-starter https://github.com/gatsbyjs/gatsby-starter-blog
+
+cd /workspaces/amestofortywo-blog
+npm install -g gatsby-cli
+npm install
+gatsby develop
